cf23e8390ab85370e0a4ad62890317166b4672a4
Rui <75488260+DayLight109@users.noreply.github.com>
2024-11-12 16:11:49 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# DNUI-Sea
-works
-大学生创新创业 海洋项目
+# sea-sunset
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true, // Optional: this can be set to false for security purposes
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
